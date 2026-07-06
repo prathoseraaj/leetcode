57a1004d67d6795da90d01e4f0e502affd26a14e
prathoseraaj V <146630697+prathoseraaj@users.noreply.github.com>
2026-07-06 13:26:45 +0530

    Time: 7 ms (27.07%), Space: 19.4 MB (99.66%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.py
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.py
@@ -5,7 +5,7 @@ class Solution:
         answer = 0
         max_end = 0
 
-        for  i,interval in enumerate(intervals):
+        for  interval in intervals:
             if interval[1] > max_end:
                 answer +=1
                 max_end = interval[1]
